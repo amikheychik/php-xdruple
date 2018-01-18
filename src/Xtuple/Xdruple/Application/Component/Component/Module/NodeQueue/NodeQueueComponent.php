84f44c2637ede95d2fb82371d457f865805c4ccd
Andrey Mikheychik <a.mikheychik@gmail.com>
2018-01-18 16:00:42 -0500

    NodeQueueComponent initialized

---
+++ src/Xtuple/Xdruple/Application/Component/Component/Module/NodeQueue/NodeQueueComponent.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Xtuple\Xdruple\Application\Component\Component\Module\NodeQueue;
+
+use Xtuple\Xdruple\Application\Component\Component;
+
+interface NodeQueueComponent
+  extends Component {
+  public const NAME = 'nodequeue';
+}
