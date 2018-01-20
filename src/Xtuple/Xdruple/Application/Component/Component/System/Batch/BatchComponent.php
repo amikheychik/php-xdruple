bb0f48f4e12e06dca572b8c2cd2e986175bcebed
Andrey Mikheychik <a.mikheychik@gmail.com>
2018-01-19 20:06:05 -0500

    BatchComponent initialized

---
+++ src/Xtuple/Xdruple/Application/Component/Component/System/Batch/BatchComponent.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Xtuple\Xdruple\Application\Component\Component\System\Batch;
+
+interface BatchComponent {
+  public const NAME = 'batch';
+
+  /**
+   * @see hook_batch_alter().
+   *
+   * @param array $batch
+   */
+  public function batchAlter(&$batch);
+
+  /**
+   * @see callback_batch_operation().
+   *
+   * @param array $args
+   */
+  public function batchOperation(&...$args);
+
+  /**
+   * @see callback_batch_finished().
+   *
+   * @param $success
+   * @param $results
+   * @param $operations
+   */
+  public function batchFinished($success, $results, $operations);
+}
