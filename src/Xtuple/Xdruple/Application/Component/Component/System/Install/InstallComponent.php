55be6ae5b01eda8f3b7777573a8f4d90fe781909
Andrey Mikheychik <a.mikheychik@gmail.com>
2018-01-21 12:51:53 -0500

    Update component mergerd into Install component
    Update component is required by Install component during installation and bind logically

--- src/Xtuple/Xdruple/Application/Component/Component/System/Install/InstallComponent.php
+++ src/Xtuple/Xdruple/Application/Component/Component/System/Install/InstallComponent.php
@@ -9,6 +9,11 @@ interface InstallComponent
   public const NAME = 'install';
 
   /**
+   * @see hook_update_N()
+   */
+  public function boot(): void;
+
+  /**
    * @see hook_schema()
    *
    * @return array
@@ -31,6 +36,31 @@ interface InstallComponent
    * @see hook_schema()
    */
   public function enable();
+
+  /**
+   * @see hook_update_last_removed()
+   *
+   * @return int
+   */
+  public function updateLastRemoved(): int;
+
+  /**
+   * @see hook_update_dependencies()
+   *
+   * @return array
+   */
+  public function updateDependencies(): array;
+
+  /**
+   * @see hook_update_N()
+   *
+   * @throws \DrupalUpdateException
+   *
+   * @param int $version
+   *
+   * @return string
+   */
+  public function updateN(int $version): string;
 
   /**
    * @see hook_disable()
